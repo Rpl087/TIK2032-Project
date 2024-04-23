fc5cedaa2c1b05cec0130ac268d5d3a17cff0270
Romal11 <115352778+Rpl087@users.noreply.github.com>
2024-04-23 14:52:36 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let text = document.getElementById("text")
 let body = document.getElementById("container")
 
 btn_img.addEventListener("click", () => {
-    if(count_click == 1) text.innerHTML = "BIODATA"
+    if(count_click == 1) text.innerHTML = "Perkenalkan nama, Romal Lengkong, dengan Program Studi Teknik Informatika, Fakultas Teknik di Universitas Sam Ratulangi"
     else if(count_click == 2) body.style.backgroundColor = "#728C8F"
     else if(count_click == 3){
         text.innerHTML = null
